09a709ae68f9e1d0143ecb708715ab37459be798
Facundo Pedrazzini <facundopedrazzinim@gmail.com>
2018-12-14 14:36:13 +1100

    test for scan checkout and fix prices on data

--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -1,16 +1,40 @@
 const assert = require('chai').assert;
 const Checkout = require('../lib/Checkout');
+const data = require('./data.mocks');
 
 describe('Checkout', () => {
 	const pricingRules = [];
 	before(() => {
-		// TODO: Add pricing rules
+		pricingRules.push(data.rules.bundle);
+		pricingRules.push(data.rules.discountPerQuantity);
+		pricingRules.push(data.rules.xForX);
+	});
+
+	let checkout;
+	beforeEach(() => {
+		checkout = new Checkout({ pricingRules });
 	});
 
 	it('should create a checkout with price rules', () => {
-		const pricingRules = [];
-		const checkout = new Checkout({ pricingRules });
-
 		assert.equal(checkout.pricingRules, pricingRules);
+	});
+
+	describe('Scan', () => {
+		it('should add item info into list of items', () => {
+			checkout.scan(data.products.appleTv);
+
+			assert.equal(checkout.items[0].sku, 'atv');
+		});
+
+		describe('XForXRule', () => {
+			it('should apply the coorect price', () => {
+				checkout.scan(data.products.appleTv);
+				checkout.scan(data.products.appleTv);
+				checkout.scan(data.products.appleTv);
+				console.log(checkout.items);
+
+				assert.equal(checkout.items[2].price, 0);
+			});
+		});
 	});
 });
