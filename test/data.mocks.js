31ee223a4e421d0c98c17975ae9496444077cd01
Facundo Pedrazzini <facundopedrazzinim@gmail.com>
2018-12-14 14:25:14 +1100

    add rules, tests and data

---
+++ test/data.mocks.js
@@ -0,0 +1,20 @@
+const Product = require('../lib/Product');
+const BundleRule = require('../lib/rules/BundleRule');
+const DiscountPerQuantityRule = require('../lib/rules/DiscountPerQuantityRule');
+const XForXRule = require('../lib/rules/XforXRule');
+const rules = require('../data/rules.json');
+const products = require('../data/products.json');
+
+module.exports = {
+	products: {
+		ipad: new Product(products.IPAD),
+		mackbookPro: new Product(products.MACKBOOK_PRO),
+		appleTv: new Product(products.APPLE_TV),
+		vgaAdapter: new Product(products.VGA_ADAPTER)
+	},
+	rules: {
+		xForX: new XForXRule(rules.X_FOR_X),
+		discountPerQuantity: new DiscountPerQuantityRule(rules.X_FOR_X),
+		bundle: new BundleRule(rules.BUNDLE)
+	}
+};
---
+++ test/rule.spec.js
@@ -0,0 +1,56 @@
+const assert = require('chai').assert;
+const BundleRule = require('../lib/rules/BundleRule');
+const DiscountPerQuantityRule = require('../lib/rules/DiscountPerQuantityRule');
+const XforXRule = require('../lib/rules/XforXRule');
+const data = require('./data.mocks');
+
+describe('Rule', () => {
+	describe('BundleRule', () => {
+		let rule;
+		before(() => {
+			rule = new BundleRule({
+				paidSkuProduct: data.products.mackbookPro.sku,
+				freeSkuProduct: data.products.vgaAdapter.sku
+			});
+		});
+
+		it('should create rule with the correct params', () => {
+			assert.equal(rule.paidSkuProduct, 'mbp');
+			assert.equal(rule.freeSkuProduct, 'vga');
+		});
+	});
+
+	describe('DiscountPerQuantityRule', () => {
+		let rule;
+		before(() => {
+			rule = new DiscountPerQuantityRule({
+				sku: data.products.ipad.sku,
+				priceWithDiscount: 499.99,
+				minimumQuantity: 4
+			});
+		});
+
+		it('should create rule with the correct params', () => {
+			assert.equal(rule.sku, 'ipd');
+			assert.equal(rule.priceWithDiscount, 499.99);
+			assert.equal(rule.minimumQuantity, 4);
+		});
+	});
+
+	describe('XforXRule', () => {
+		let rule;
+		before(() => {
+			rule = new XforXRule({
+				paidQuantity: 2,
+				totalQuantity: 3,
+				sku: data.products.appleTv.sku
+			});
+		});
+
+		it('should create rule with the correct params', () => {
+			assert.equal(rule.paidQuantity, 2);
+			assert.equal(rule.totalQuantity, 3);
+			assert.equal(rule.sku, 'atv');
+		});
+	});
+});
