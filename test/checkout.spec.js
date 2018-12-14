d997d422d2129cec79b20f5daa0dfab7d70bbbe9
Facundo Pedrazzini <facundopedrazzinim@gmail.com>
2018-12-14 15:56:21 +1100

    test for example scenarios

--- test/checkout.spec.js
+++ test/checkout.spec.js
@@ -10,20 +10,47 @@ describe('Checkout', () => {
 		pricingRules.push(data.rules.xForX);
 	});
 
-	let checkout;
-	beforeEach(() => {
-		checkout = new Checkout({ pricingRules });
-	});
-
 	it('should create a checkout with price rules', () => {
+		const checkout = new Checkout({ pricingRules });
+
 		assert.equal(checkout.pricingRules, pricingRules);
 	});
 
 	describe('Scan', () => {
 		it('should add item info into list of items', () => {
+			const checkout = new Checkout({ pricingRules });
+
 			checkout.scan(data.products.appleTv);
 
 			assert.equal(checkout.items[0].sku, 'atv');
+		});
+	});
+
+	describe('Total', () => {
+		let checkout;
+		beforeEach(() => {
+			checkout = new Checkout({ pricingRules });
+		});
+
+		it('should work the first example scenario', () => {
+			checkout.scan(data.products.appleTv);
+			checkout.scan(data.products.appleTv);
+			checkout.scan(data.products.appleTv);
+			checkout.scan(data.products.vgaAdapter);
+
+			assert.equal(checkout.total(), 249);
+		});
+
+		it('should work the second example scenario', () => {
+			checkout.scan(data.products.appleTv);
+			checkout.scan(data.products.ipad);
+			checkout.scan(data.products.ipad);
+			checkout.scan(data.products.appleTv);
+			checkout.scan(data.products.ipad);
+			checkout.scan(data.products.ipad);
+			checkout.scan(data.products.ipad);
+
+			assert.equal(checkout.total(), 2718.95);
 		});
 	});
 });
